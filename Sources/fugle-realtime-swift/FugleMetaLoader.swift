58f5977289c9cdd7ae5b10d9e3836886106e7ba3
Dan Hung <dan12411@gmail.com>
2022-11-06 12:58:50 +0800

    feat: implement `load()`.

--- Sources/fugle-realtime-swift/FugleMetaLoader.swift
+++ Sources/fugle-realtime-swift/FugleMetaLoader.swift
@@ -4,7 +4,20 @@ public class FugleMetaLoader {
 
     public init() {}
 
-    public func load() -> Result<Meta, Error> {
-        return .success(Meta())
+    public func load(token: String, symbolId: String) async -> Result<Intraday<MetaData>, Error> {
+        var urlComponents = URLComponents()
+        urlComponents.host = "api.fugle.tw"
+        urlComponents.scheme = "https"
+        urlComponents.path = "/realtime/v0.3/intraday/meta"
+        urlComponents.query = "symbolId=\(symbolId)&apiToken=\(token)"
+        let url = urlComponents.url!
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let meta = try JSONDecoder().decode(Intraday<MetaData>.self, from: data)
+            return .success(meta)
+        } catch {
+            return .failure(error)
+        }
     }
 }
