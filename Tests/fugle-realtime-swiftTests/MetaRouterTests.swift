f1ad0af19b816dadbe10c579b75a83367afc478f
Dan Hung <dan12411@gmail.com>
2022-11-26 22:14:52 +0800

    feat: inject `symbolId` & `apiToken`.

--- Tests/fugle-realtime-swiftTests/MetaRouterTests.swift
+++ Tests/fugle-realtime-swiftTests/MetaRouterTests.swift
@@ -2,6 +2,10 @@ import XCTest
 
 final class MetaRouter {
 
+    let symbolId: String
+
+    let apiToken: String
+
     let scheme: String = "https"
 
     let host: String = "api.fugle.tw"
@@ -13,15 +17,21 @@ final class MetaRouter {
         components.scheme = scheme
         components.host = host
         components.path = basePath + "/meta"
+        components.query = "symbolId=\(symbolId)&apiToken=\(apiToken)"
         return components.url!
+    }
+
+    init(symbolId: String, apiToken: String) {
+        self.symbolId = symbolId
+        self.apiToken = apiToken
     }
 }
 
 final class MetaRouterTests: XCTestCase {
 
     func testMetaRouter() throws {
-        let sut = MetaRouter()
+        let sut = MetaRouter(symbolId: "5566", apiToken: "taiwanNo1")
 
-        XCTAssertEqual(sut.url, URL(string: "https://api.fugle.tw/realtime/v0.3/intraday/meta"))
+        XCTAssertEqual(sut.url, URL(string: "https://api.fugle.tw/realtime/v0.3/intraday/meta?symbolId=5566&apiToken=taiwanNo1"))
     }
 }
