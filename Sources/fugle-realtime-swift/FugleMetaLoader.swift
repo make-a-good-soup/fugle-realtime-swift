4661b3fe40b6baba253c58b4e9c79d4d7404acde
Dan Hung <dan12411@gmail.com>
2022-11-26 22:46:14 +0800

    feat: use `IntradayRouter.meta` in loader.

--- Sources/fugle-realtime-swift/FugleMetaLoader.swift
+++ Sources/fugle-realtime-swift/FugleMetaLoader.swift
@@ -5,12 +5,7 @@ public class FugleMetaLoader {
     public init() {}
 
     public func load(token: String, symbolId: String) async -> Result<Intraday<MetaData>, Error> {
-        var urlComponents = URLComponents()
-        urlComponents.host = "api.fugle.tw"
-        urlComponents.scheme = "https"
-        urlComponents.path = "/realtime/v0.3/intraday/meta"
-        urlComponents.query = "symbolId=\(symbolId)&apiToken=\(token)"
-        let url = urlComponents.url!
+        let url = IntradayRouter.meta(symbolId: symbolId, apiToken: token).url
 
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
