c0d31a84d1020f560f8622213ae25442f9ad048b
Dan Hung <dan12411@gmail.com>
2022-12-06 23:08:06 +0800

    fix: make model public access

--- Sources/fugle-realtime-swift/Models/Intraday.swift
+++ Sources/fugle-realtime-swift/Models/Intraday.swift
@@ -1,13 +1,13 @@
 import Foundation
 
 public struct Intraday<T: Codable>: Codable {
-    let apiVersion: String
-    let data: T
+    public let apiVersion: String
+    public let data: T
 }
 
 public struct MetaData: Codable {
-    let info: Info
-    let meta: Meta
+    public let info: Info
+    public let meta: Meta
 }
 
 public struct ChartData: Codable {
--- Sources/fugle-realtime-swift/Models/Meta.swift
+++ Sources/fugle-realtime-swift/Models/Meta.swift
@@ -1,21 +1,21 @@
 import Foundation
 
-struct Meta: Codable {
-    let market: String
-    let nameZhTw: String
-    let industryZhTw: String
-    let priceReference: Double
-    let priceHighLimit: Double
-    let priceLowLimit: Double
-    let canDayBuySell: Bool
-    let canDaySellBuy: Bool
-    let canShortMargin: Bool
-    let canShortLend: Bool
-    let tradingUnit: Int
-    let currency: String
-    let isTerminated: Bool
-    let isSuspended: Bool
-    let typeZhTw: String
-    let abnormal: String
-    let isUnusuallyRecommended: Bool
+public struct Meta: Codable {
+    public let market: String
+    public let nameZhTw: String
+    public let industryZhTw: String?
+    public let priceReference: Double
+    public let priceHighLimit: Double
+    public let priceLowLimit: Double
+    public let canDayBuySell: Bool
+    public let canDaySellBuy: Bool
+    public let canShortMargin: Bool?
+    public let canShortLend: Bool?
+    public let tradingUnit: Int
+    public let currency: String?
+    public let isTerminated: Bool
+    public let isSuspended: Bool
+    public let typeZhTw: String
+    public let abnormal: String
+    public let isUnusuallyRecommended: Bool?
 }
