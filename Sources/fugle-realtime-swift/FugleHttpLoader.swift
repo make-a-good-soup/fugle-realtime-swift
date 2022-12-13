98adea7ddfbd0396b6372e48713512ef2f0b86cf
Nick Chen <nick45chen@gmail.com>
2022-12-13 17:56:18 +0800

    changed: rename FugleMetaLoader to FugleHttpLoader (#12)

--- Sources/fugle-realtime-swift/FugleHttpLoader.swift
+++ Sources/fugle-realtime-swift/FugleHttpLoader.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-public class FugleMetaLoader {
-
+public class FugleHttpLoader {
+    
     public init() {}
-
-    public func load(token: String, symbolId: String) async -> Result<Intraday<MetaData>, Error> {
+    
+    public func loadMeta(token: String, symbolId: String) async -> Result<Intraday<MetaData>, Error> {
         let parameters = ["symbolId": symbolId, "apiToken": token]
         let url = IntradayRouter.meta(parameters: parameters).url
-
+        
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
             let meta = try JSONDecoder().decode(Intraday<MetaData>.self, from: data)
