1137870387f11abc29c24e51f84ee9d7356d91e1
nick chen <nick45chen@gmail.com>
2022-12-14 00:46:05 +0800

    add: CodingKeys

--- Sources/fugle-realtime-swift/Models/Chart.swift
+++ Sources/fugle-realtime-swift/Models/Chart.swift
@@ -1,11 +1,21 @@
 import Foundation
 
 struct Chart : Codable {
-    let a : [Double]? // 當日個股於此分鐘的成交均價
-    let o : [Double]? // 此分鐘的開盤價
-    let h : [Double]? // 此分鐘的最高價
-    let l : [Double]? // 此分鐘的最低價
-    let c : [Double]? // 此分鐘的收盤價
-    let v : [Double]? // 此分鐘的成交量 (指數：金額；個股：張數；興櫃股票及零股：股數)
-    let t : [Double]? // Unix timestamp (每分鐘單位)
+    let averagePrices : [Double]? // 當日個股於此分鐘的成交均價
+    let openingPrices : [Double]? // 此分鐘的開盤價
+    let highestPrices : [Double]? // 此分鐘的最高價
+    let lowestPrices : [Double]? // 此分鐘的最低價
+    let closingPrices : [Double]? // 此分鐘的收盤價
+    let volumes : [Double]? // 此分鐘的成交量 (指數：金額；個股：張數；興櫃股票及零股：股數)
+    let times : [Double]? // Unix timestamp (每分鐘單位)
+    
+    enum CodingKeys: String, CodingKey {
+        case averagePrices = "a"
+        case openingPrices = "o"
+        case highestPrices = "h"
+        case lowestPrices = "l"
+        case closingPrices = "c"
+        case volumes = "v"
+        case times = "t"
+    }
 }
