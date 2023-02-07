3f96813a2af0c393621b972d456bd3160a71ae3a
Dan Hung <dan12411@gmail.com>
2023-02-07 21:13:52 +0800

    feat: modify demo app.

--- Examples/Realtime Demo/Realtime Demo/ContentView.swift
+++ Examples/Realtime Demo/Realtime Demo/ContentView.swift
@@ -1,44 +1,118 @@
 import SwiftUI
 import fugle_realtime_swift
 
+private enum DataType: String, CaseIterable {
+    case meta
+    case quote
+    case chart
+
+    var title: String {
+        switch (self) {
+        case .meta: return "Meta"
+        case .quote: return "Quote"
+        case .chart: return "Chart"
+        }
+    }
+
+    var buttonTitle: String { "Get \(title)" }
+}
+
 struct ContentView: View {
+    private var symbolId: String { "2884" }
+
     private var apiToken: String { "demo" }
 
-    @State var stockName: String = "stockName"
-    @State var stockPrice: Double = 0.0
+    @State var title: String = ""
+
+    @State var jsonString: String = "Tap Button to print data"
+
+    private let types = DataType.allCases
     
     var body: some View {
         VStack {
-            Button("Hello, world!") {
-                Task {
-                    let result = await FugleHttpLoader().loadMeta(token: apiToken, symbolId: "2884")
-                    let model = try? result.get()
-                    if let model {
-                        print(model)
+            Text(title)
+                .font(.title)
+                .frame(height: 50)
+            ScrollView {
+                Text(jsonString)
+                    .modifier(ConsoleLabel())
+            }
+            .modifier(ConsoleView())
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(DataType.allCases, id: \.rawValue) { type in
+                        Button(type.buttonTitle) {
+                            title = type.title
+                            jsonString = "Loading..."
+                            loadData(type: type)
+                        }.modifier(LoadButton())
                     }
-                }
-
-            }.padding(8)
-            Text(stockName)
-                .font(.system(size: 24))
-                .padding(8)
-            Text("$\(String(stockPrice))")
-                .font(.system(size: 18))
-                .padding(8)
-        }.task {
-            let parameters = ["symbolId": "2884", "apiToken": apiToken]
-            let stream = WebSocketStream(url: WebSocketRouter.meta(parameters: parameters).url)
-
-            do {
-                for try await message in stream {
-                    let meta = try message.meta()
-                    stockName = meta.data.meta.nameZhTw
-                    stockPrice = meta.data.meta.priceReference
-                }
-            } catch {
-                debugPrint("Oops something didn't go right")
+                }.padding([.horizontal])
             }
         }
+    }
+
+    private func loadData(type: DataType) {
+        title = type.title
+        Task {
+            let model = await getModel(type: type)
+            updateJSONString(model)
+        }
+    }
+
+    private func getModel(type: DataType) async -> Encodable {
+        switch type {
+        case .meta:
+            let result = await FugleHttpLoader().loadMeta(token: apiToken, symbolId: symbolId)
+            return try? result.get()
+        case .quote:
+            let result = await FugleHttpLoader().loadQuote(token: apiToken, symbolId: symbolId)
+            return try? result.get()
+        case .chart:
+            let result = await FugleHttpLoader().loadChart(token: apiToken, symbolId: symbolId)
+            return try? result.get()
+        }
+    }
+
+    private func updateJSONString(_ model: Encodable) {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        let data = try? encoder.encode(model)
+        if let data {
+            jsonString = String(data: data, encoding: .utf8) ?? "Fail to load data."
+        }
+    }
+}
+
+private struct ConsoleLabel: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.body)
+            .padding()
+            .background(.black)
+            .foregroundColor(.white)
+    }
+}
+
+private struct ConsoleView: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: .infinity)
+            .background(.black)
+            .cornerRadius(10)
+            .padding([.bottom, .horizontal])
+    }
+}
+
+private struct LoadButton: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.headline)
+            .padding()
+            .frame(width: 120, height: 44)
+            .background(.teal)
+            .foregroundColor(.white)
+            .cornerRadius(44)
     }
 }
 
