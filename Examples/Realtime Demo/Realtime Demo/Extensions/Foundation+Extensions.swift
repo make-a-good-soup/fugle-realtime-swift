656bce57c87cf420473fc09ffa442c3f278e6949
Dan <dan12411@fugle.tw>
2023-04-18 20:57:11 +0800

    fix: x-axis value.

--- Examples/Realtime Demo/Realtime Demo/Extensions/Foundation+Extensions.swift
+++ Examples/Realtime Demo/Realtime Demo/Extensions/Foundation+Extensions.swift
@@ -5,3 +5,16 @@ extension Double {
         String(format: "%.2f", self)
     }
 }
+
+extension Date {
+
+    func dateComponents(timeZone: TimeZone, calendar: Calendar = .current) -> DateComponents {
+        let current = calendar.dateComponents(in: timeZone, from: self)
+
+        var dc = DateComponents(timeZone: timeZone, year: current.year, month: current.month)
+        dc.hour = current.hour
+
+        return dc
+    }
+
+}
